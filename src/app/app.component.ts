339070b24a5c13680b94f0bad8fbe2a5ed159196
Aneela Brister <aneelabrister@hotmail.com>
2023-07-31 13:40:58 -0700

    Asking more questions; clearing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { BehaviorSubject, EMPTY, Observable, flatMap, from, map, mergeMap, of, startWith, tap } from 'rxjs';
+import { Component, ElementRef, QueryList, ViewChild } from '@angular/core';
+import { BehaviorSubject, EMPTY, Observable, flatMap, from, map, mergeMap, of, shareReplay, startWith, tap, zip } from 'rxjs';
 import { WebScraperService } from './web-scraper.service';
 import { OpenAiService } from './open-ai.service';
 import { getCurrentTab, isChrome } from '../background';
@@ -18,9 +18,13 @@ export class AppComponent {
 
   url$: BehaviorSubject<any> = new BehaviorSubject('');
   thinking$: BehaviorSubject<boolean> = new BehaviorSubject(false);
-  scraped$: Observable<string> = EMPTY;
+  fetched$: Observable<string> = EMPTY;
+  objectivity$: Observable<string> = EMPTY;
+  summary$: Observable<string> = EMPTY;
+  clarity$: Observable<string> = EMPTY;
+  sophistication$: Observable<string> = EMPTY;
   isChromeExt$: BehaviorSubject<boolean> = new BehaviorSubject(true);
-
+  @ViewChild('url', {static: false}) inputControl: ElementRef = {} as ElementRef;
 
   ngOnInit() {
     this.isChromeExt$.next(isChrome());
@@ -31,25 +35,51 @@ export class AppComponent {
         this.mediapedia(this.url$.value);
       });
     }
-  }
 
-  mediapedia(url: string) {
-    this.thinking$.next(true);
-    this.scraped$ = this._http.get<string>('fetch', {
-      params: {url: url}
-    }).pipe(
+    this.fetched$ = this.url$.pipe(
+      mergeMap((url: string) => url ? this._http.get<string>('fetch', {
+        params: {url: url}
+      }) : of('') ),
       map((s: string|void) => s ? s : ''),
       tap(s => console.log('scraped text', s)),
+      shareReplay(1)
+    );
+    this.summary$ = this.fetched$.pipe(
       mergeMap((scraped: string) => {
-        return this._llm.doAPrompt(scraped);
+        return scraped ? this._llm.summary(scraped) : of('');
+      })
+    );
+    this.objectivity$ = this.fetched$.pipe(
+      mergeMap((scraped: string) => {
+        return scraped ? this._llm.objectivity(scraped) : of('');
+      })
+    );
+    this.sophistication$ = this.fetched$.pipe(
+      mergeMap((scraped: string) => {
+        return scraped ? this._llm.sophistication(scraped) : of('');
+      })
+    );
+    this.clarity$ = this.fetched$.pipe(
+      mergeMap((scraped: string) => {
+        return scraped ? this._llm.clarity(scraped) : of('');
       }),
       tap(s => {
-        console.log('answer', s);
         this.thinking$.next(false);
       })
     );
+
+    zip(this.summary$, this.objectivity$, this.sophistication$, this.clarity$).subscribe(() => this.thinking$.next(false));
   }
 
+  mediapedia(url: string) {    
+    this.thinking$.next(true);
+    this.url$.next(url);
+  }
 
+  clear() {
+    this.url$.next('');
+    this.inputControl.nativeElement.value = '';
+    this.inputControl.nativeElement.focus();
+  }
 
 }
